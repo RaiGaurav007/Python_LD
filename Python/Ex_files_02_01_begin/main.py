4f2a049dc4bd14b1b1f7c1b0c11007d8bc53aff0
RaiGaurav007 <43960995+RaiGaurav007@users.noreply.github.com>
2026-08-13 17:34:42 +0000

    Ex_files_02_01_begin

---
+++ Python/Ex_files_02_01_begin/main.py
@@ -0,0 +1,12 @@
+RUN_INDENTED = False
+
+message = "running unindented"
+
+if RUN_INDENTED:
+    message = "running indented"
+print(message)
+
+def my_function():
+    greet = "Hello"
+    return greet
+print(my_function())
